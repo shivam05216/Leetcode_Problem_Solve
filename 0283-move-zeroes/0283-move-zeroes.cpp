3c8059ab232b995a2a79e87b27b450090fc55cd8
Shivam Tiwari <shivamtishivamti05@gmail.com>
2026-07-19 23:38:41 +0530

    Time: 0 ms (100%), Space: 23.7 MB (83.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j=0;  
+        /*int j=0;  
         for(int i=0;i<nums.size();i++){
             if (nums[i]!=0){
               
@@ -9,6 +9,19 @@ public:
             
                 j++;  
             }
+        }*/
+        int k = 0;
+
+        for(int i=0; i<nums.size(); i++){
+
+
+            if(nums[i]!= 0){
+                nums[k++] = nums[i];
+            }
+        }
+
+        while (k < nums.size()){
+            nums[k++] = 0;
         }
     }
 };
