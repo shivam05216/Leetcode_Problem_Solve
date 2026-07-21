ee5edb80970d71b06106923f767ce6983baa14cd
Shivam Tiwari <shivamtishivamti05@gmail.com>
2026-07-22 01:44:06 +0530

    Time: 0 ms (100%), Space: 13.3 MB (79.46%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     
     bool searchinrow(vector<vector<int>>& matrix, int target, int row) {
@@ -41,5 +41,31 @@ public:
             }
         }
         return false;
+    }
+};*/
+
+// solution of search in 2d matrix of 2
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m= matrix.size(); int n= matrix[0].size();
+
+        int r=0, c= n-1;
+        while (c>=0 && r<m){
+            if (target== matrix[r][c])
+            {
+                return true;
+            }
+            else if (target < matrix[r][c]){
+                c--;
+            }
+            else {
+                r++;
+            }
+
+        }
+        return false;
+
     }
 };
