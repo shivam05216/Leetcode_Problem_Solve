9ab10064c684d11eb2b9ea12a603b97e8298c62a
Shivam Tiwari <shivamtishivamti05@gmail.com>
2026-07-24 03:52:31 +0530

    Time: 0 ms (100%), Space: 40.2 MB (81.58%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -16,28 +16,26 @@ public:
 // calculate pefix and suffix product for all the indexes using separate loopsand then multiply them for eaxh index
     
        vector<int> ans(nums.size(), 1);
-       vector<int> prefix(nums.size(), 1);
-       vector<int> sufix(nums.size(), 1);
+      
        //prefix
        int n=nums.size();
 
-       prefix[0]=1;
+      
 
        for (int i=1; i<n; i++){
-        prefix[i]= prefix[i-1]*nums[i-1];
+        ans[i] *= ans[i-1]*nums[i-1];
        }
 
        // sufix
-       sufix[n-1]=1;
+
+       int sufix =1;
        for (int i=n-2; i>=0; i--){
-        sufix[i]= sufix[i+1]*nums[i+1];
+        sufix*= nums[i+1];
+        ans[i]*=sufix;
        }
 
        // ans
 
-       for(int i=0; i<n; i++){
-        ans[i]= prefix[i]*sufix[i];
-       }
 
        return ans;
    
